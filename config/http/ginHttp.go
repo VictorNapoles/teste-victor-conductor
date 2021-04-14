56961dc11ec27c7f30670ef5a8004c30966c8cf3
Victor Nápoles <victornapoles@MacBook-Pro-de-Victor.local>
2021-04-14 16:43:21 -0300

    Gin Framework

---
+++ config/http/ginHttp.go
@@ -0,0 +1,22 @@
+package http
+
+import "github.com/gin-gonic/gin"
+
+var (
+	server *gin.Engine
+	router *gin.RouterGroup
+)
+
+func init() {
+	server = gin.Default()
+	router = server.Group("/conductor/v1/api/")
+
+}
+
+func GetServer() *gin.Engine {
+	return server
+}
+
+func GetRouter() *gin.RouterGroup {
+	return router
+}
