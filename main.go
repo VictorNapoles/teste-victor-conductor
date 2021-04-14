ade545d34134859a2cc0ea2fb8783c803bdee953
Victor Nápoles <victornapoles@MacBook-Pro-de-Victor.local>
2021-04-14 16:45:27 -0300

    Start da aplicação

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
 	_ "github.com/VictorNapoles/teste-victor-conductor/adapter/crud"
 	_ "github.com/VictorNapoles/teste-victor-conductor/config"
 	_ "github.com/VictorNapoles/teste-victor-conductor/config/database"
+	_ "github.com/VictorNapoles/teste-victor-conductor/config/http"
+	conductor "github.com/VictorNapoles/teste-victor-conductor/config/http"
+	_ "github.com/VictorNapoles/teste-victor-conductor/controller"
+	_ "github.com/VictorNapoles/teste-victor-conductor/service"
 )
 
 func main() {
-
+	conductor.GetServer().Run()
 }
